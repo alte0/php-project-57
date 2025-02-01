e390845f5d7591952265adb8f8a9d84eb47ca6dd
Максим Дмитриев <makc-dmv@yandex.ru>
2025-02-01 23:17:42 +0500

    fixed try html for hexlet test

--- resources/views/task_status/index.blade.php
+++ resources/views/task_status/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @if (session('status'))
-        <div class="alert alert-success">
+        <div class="alert alert-success" role="alert">
             {{ session('status') }}
         </div>
     @endif
