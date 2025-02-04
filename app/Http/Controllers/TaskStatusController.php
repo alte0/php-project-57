fdb52e99d8a5df40162d281c0bb473c7a63ea596
Максим Дмитриев <makc-dmv@yandex.ru>
2025-02-04 10:20:22 +0500

    added dd

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -86,7 +86,7 @@ class TaskStatusController extends Controller
         $this->ensureAuthorized();
 
         $hasTasks = Task::query()->where('status_id', $taskStatus->id)->exists();
-        dd($taskStatus);
+        dd($taskStatus->toArray());
 
         if ($hasTasks) {
             $message = trans('task_manager.messages.removedError');
