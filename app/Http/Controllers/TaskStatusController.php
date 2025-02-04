1d7189618b3c7f4c9703926f99762284e8b316cb
Максим Дмитриев <makc-dmv@yandex.ru>
2025-02-04 10:55:24 +0500

    fixed try TaskStatusController.php for hexlet test

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -22,7 +22,7 @@ class TaskStatusController extends Controller
      */
     public function index()
     {
-        return view('task_status.index', ['statuses' => TaskStatus::query()->orderBy('name')->get()]);
+        return view('task_status.index', ['statuses' => TaskStatus::query()->orderBy('created_at')->get()]);
     }
 
     /**
