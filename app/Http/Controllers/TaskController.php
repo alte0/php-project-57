116f52ef0235933bafb188330c679650c59810d6
Максим Дмитриев <makc-dmv@yandex.ru>
2025-02-09 12:32:18 +0500

    hexlet test: fixed redirect for delete task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -167,6 +167,6 @@ class TaskController extends Controller
             $message = trans('task_manager.messagesTask.removedError');
         }
 
-        return redirect()->back()->with('message', $message);
+        return redirect()->route('tasks.index')->with('message', $message);
     }
 }
